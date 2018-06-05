47bb7696e841f873b66d71433fd43e96e5d3bf06
MEHAVARNAN <MEHAVARNAN@MEHAVARNAN.mshome.net>
2018-06-05 22:55:51 +0530

    Updated extent report path

--- src/test/java/utilities/ExtentFactory.java
+++ src/test/java/utilities/ExtentFactory.java
@@ -6,7 +6,7 @@ public class ExtentFactory {
 	
 	public static ExtentReports getInstance() {
 		ExtentReports report;
-		String path = "C:\\Users\\MEHAVARNAN\\git\\hrmdemo\\hrmdemo\\extentreports\\report.html";
+		String path = "C:\\Users\\MEHAVARNAN\\git\\hrmdemo\\extentreports\\report.html";
 		report = new ExtentReports(path, true);
 		report.addSystemInfo("Selenium Version", "3.11");
 		report.addSystemInfo("Platform", "Windows10");
